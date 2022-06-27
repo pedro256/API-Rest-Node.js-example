4e4c4afb0c7d0d588748a460809c71bd06f7bf0e
Pedro Henrique <pedroh93601@gmail.com>
2022-06-27 12:08:43 -0400

    typeorm settings

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import '@shared/typeorm'
 
 const app = express();
 
