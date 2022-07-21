291a58a5000877d6b64c9a6dfff7cfec51ee8326
pedro256 <pedroh93601@gmail.com>
2022-07-20 22:12:44 -0400

    Validation Calebrate Product

--- src/modules/Products/routes/products.routes.ts
+++ src/modules/Products/routes/products.routes.ts
@@ -1,12 +1,49 @@
 import { Router } from "express";
+import { celebrate, Joi, Segments } from 'celebrate'
 import ProductsController from "../controllers/ProductsController";
 const productsRouter = Router();
 const productsController = new ProductsController();
 
-productsRouter.get('/',productsController.index);
-productsRouter.get('/:id',productsController.show);
-productsRouter.post('/',productsController.create);
-productsRouter.put('/:id',productsController.update);
-productsRouter.delete('/:id',productsController.delete);
+productsRouter.get('/', productsController.index);
+productsRouter.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.number().integer().min(1).required()
+        }
+    })
+    , productsController.show);
+
+productsRouter.post('/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            description: Joi.string().min(0),
+            price: Joi.number().min(0.1).required(),
+            quantity:  Joi.number().integer().min(0).default(0)
+        }
+    })
+    , productsController.create);
+productsRouter.put(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.number().integer().min(1).required()
+        },
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            description: Joi.string(),
+            price: Joi.number().min(0.1).required(),
+            quantity: Joi.number().integer().min(0).default(0)
+        }
+    })
+    , productsController.update);
+productsRouter.delete('/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.number().integer().min(1).required()
+        }
+    })
+    , productsController.delete);
 
 export default productsRouter;
