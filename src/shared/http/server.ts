b0844a1adbd639a983582625f2c10da452c56245
pedro256 <pedroh93601@gmail.com>
2022-07-20 22:11:58 -0400

    Config Celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express';
+import {errors} from 'celebrate'
 import cors from 'cors';
 import routes from './routes';
 import '@shared/typeorm'
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(routes);
+app.use(errors());
 app.use(
     (error:Error,request:Request,response:Response,next:NextFunction)=>{
         if(error instanceof AppError){
