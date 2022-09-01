593ccb3bde90e79e9d048416290607e82d793795
Pedro256 <pedroh93601@gmail.com>
2022-09-01 15:18:41 -0400

    fix: removing user password

--- src/modules/Users/controllers/UserController.ts
+++ src/modules/Users/controllers/UserController.ts
@@ -1,3 +1,4 @@
+import { classToClass } from "class-transformer";
 import { Request, Response } from "express";
 import CreateUserService from "../services/CreateUserService";
 import ListUsersService from "../services/ListUsersService";
@@ -8,7 +9,8 @@ export default class UserController {
     public async index(req:Request,res:Response):Promise<Response>{
         const listUser = new ListUsersService();
         const users = await listUser.execute();
-        return res.json(users);
+
+        return res.json(classToClass(users));
     }
 
     public async create(req:Request,res:Response):Promise<Response>{
