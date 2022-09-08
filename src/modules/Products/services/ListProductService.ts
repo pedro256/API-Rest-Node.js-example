141e5f6202891747a4d53bbe3f66b88370fbabed
Pedro256 <pedroh93601@gmail.com>
2022-09-08 12:30:29 -0400

    feat: redis implementation

--- src/modules/Products/services/ListProductService.ts
+++ src/modules/Products/services/ListProductService.ts
@@ -1,11 +1,18 @@
 import { getCustomRepository } from "typeorm";
 import { ProductEntity } from "../typeorm/entities/ProductEntity";
 import { ProductRepository } from "../typeorm/repositories/ProductRepository";
+import RedisCache from "@shared/cache-service/RedisCache";
 
 export default class ListProductService{
     public async execute():Promise<ProductEntity[]>{
         const productsRepository = getCustomRepository(ProductRepository);
-        const products = productsRepository.find();
+        const redisCache = new RedisCache();
+        let products = await redisCache.recover<ProductEntity[]>('PRODUCTS-LS');
+        if(!products){
+            products = await productsRepository.find();
+            await redisCache.save('PRODUCTS-LS',products)
+        }
+        //await redisCache.save('teste','teste')
         return products;
 
     }
